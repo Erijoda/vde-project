69e905daabd7eb5238919fefd06612254050a48f
Erijoda <erijoda1991@gmail.com>
2017-05-27 18:58:26 +0200

    Edited dbconfig

--- config/db.config.php
+++ config/db.config.php
@@ -9,7 +9,7 @@ return array(
   'options'  => [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false
+    PDO::ATTR_EMULATE_PREPARES   => false,
   ],
-  'salt'     => '141254da!.;sd2'
+  'salt'     => '141254da!.;sd2',
 );
