f000e463ce7dc6f4ee724d30edae9540fc0631fd
Erijoda <erijoda1991@gmail.com>
2017-05-27 19:04:11 +0200

    Error handling for dbconnection

--- db-connection.class.php
+++ db-connection.class.php
@@ -3,6 +3,10 @@
 class DBConnection extends PDO {
   public function __construct() {
     $config = include('config/db.config.php');
-    parent::__construct("mysql:host={$config['host']};dbname={$config['db']};charset={$config['charset']}", $config['username'], $config['password'], $config['options']);
+    try {
+      parent::__construct("mysql:host={$config['host']};dbname={$config['db']};charset={$config['charset']}", $config['username'], $config['password'], $config['options']);
+    } catch(PDOException $e) {
+      echo "Un unexpected error occured when trying to connect to the database.<br>{$e->getMessage()}";
+    }
   }
 }
